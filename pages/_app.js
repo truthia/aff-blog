ed48612f038af313fb16f3492d1da167bcc387f0
Trung Trần <trung247vn@gmail.cm>
2022-06-15 17:47:41 +0700

    fix bugs

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
+import styled from 'styled-components'
 import { Layout } from '../components'
 import '../styles/globals.scss'
+
+const BackGround= styled.div`
+   position:absolute;
+  left:0;
+  top:0;
+  width:100%;
+  height:100%;
+  background-color: black;
+  z-index: 99999;  /*just make sure it's the highest on the page*/
+`
 
 function MyApp({ Component, pageProps }) {
   return (
